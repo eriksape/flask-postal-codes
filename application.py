a80c9119ea6fda69657a8be4fbe5d538e97fc4ce
Erik Sanchez <erik.sape@gmail.com>
2020-07-10 20:27:40 -0500

    refactor(404): change 404 to json response

--- application.py
+++ application.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from os import environ as env
+
 
 def create_app(config_filename):
     application = Flask(__name__)
     application.config.from_object(config_filename)
-    
+
     from app import api_bp
     application.register_blueprint(api_bp)
 
@@ -15,10 +16,16 @@ def create_app(config_filename):
 
     return application
 
+
 application = create_app("config")
 
+
+@application.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+
+
 if __name__ == "__main__":
-    
     debug = (env.get("DEBUG") == 'true')
     port = int(env.get("PORT", 5000))
     host = env.get("HOST", '127.0.0.1')
